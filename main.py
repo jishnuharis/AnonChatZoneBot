691fbf7162a666322d9daab1cd1deee11a40b248
Smooothie008 <smooothie008@gmail.com>
2026-03-14 22:03:20 +0530

    structure overwork

--- main.py
+++ main.py
@@ -68,10 +68,11 @@ async def on_shutdown(application):
 
 
 # Main function to keep the bot alive, handle user commands and user inputs, relaying messages between users
-def main():
+async def main():
     keep_alive()  # Keeps the bot alive
 
-    app = ApplicationBuilder().token(init.BOT_TOKEN).post_init(set_commands).post_shutdown(on_shutdown).build()  # The app which makes the bot work
+    app = ApplicationBuilder().token(init.BOT_TOKEN).post_shutdown(on_shutdown).build()  # The app which makes the bot work
+    await set_commands(app)
 
     app.add_handler(CommandHandler("start", start))  # Connects the 'start' command to its functionality
     app.add_handler(CommandHandler("find", find))  # Connects the 'find' command to its functionality
@@ -92,11 +93,12 @@ def main():
 
     app.add_error_handler(global_error_handler)
 
-    app.create_task(periodic_save())  # Saves the user data
-    app.create_task(periodic_feedback_clear())  # Frees up the feedback_track
-    app.run_polling()  # Runs the app
-
+    asyncio.create_task(periodic_save())  # Saves the user data
+    asyncio.create_task(periodic_feedback_clear())  # Frees up the feedback_track
+    await app.run_polling()  # Runs the app
 
 # Part which keeps the event loop running
 if __name__ == '__main__':
-    main()
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
