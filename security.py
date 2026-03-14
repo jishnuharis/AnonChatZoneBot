0c428aeac406f52e8e012b2e3679fcb6b1366096
Smooothie008 <smooothie008@gmail.com>
2026-03-14 22:34:24 +0530

    structure overwork

--- security.py
+++ security.py
@@ -1,4 +1,4 @@
-from telegram.error import Forbidden  # Importing the 'Forbidden' exception
+from telegram.error import Forbidden, Conflict  # Importing the 'Forbidden' exception
 
 import init
 
@@ -14,11 +14,13 @@ async def safe_tele_func_call(caller, *args, **kwargs):
 async def global_error_handler(update, context):
     try:
         e = context.error
-        text = f" 🚨 YO THERE IS AN ERROR TWIN 🚨 \n\n{type(e).__name__}: {e}\n"
 
+        if isinstance(e, Conflict):
+            return
+
+        text = f" 🚨 YO THERE IS AN ERROR TWIN 🚨 \n\n{type(e).__name__}: {e}\n"
         if update and update.effective_user:
             text += f"\n👤 User ID: {update.effective_user.id}"
-
         text += "\n\nngl something just exploded 😭 pls come check...!"
 
         await context.bot.send_message(chat_id=init.OWNER, text=text)
